b5e96494e96490a85389f905f176029452870147
JenVest2020 <jenvest2019@gmail.com>
2020-09-22 22:23:06 -0500

    MVP completed

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -13,13 +13,36 @@ function findById(id) {
 function findSteps(id) {
     return db('steps as s')
         .join('schemes as m', 'm.id', 's.scheme_id')
-        .orderBy('s.id');
+        .select('s.id', 'm.scheme_name', 's.step_number', 's.instructions')
+        .orderBy('s.step_number');
+}
+
+function add(schemeData) {
+    return db('schemes')
+        .insert(schemeData, 'id');
+}
+
+// function addStep(stepData, id) {
+//     return db('')
+// }
+
+function update(changes, id) {
+    return db('schemes')
+        .where({ id })
+        .update(changes);
+}
+
+function remove(id) {
+    return db('schemes')
+        .where({ id })
+        .del();
 }
 module.exports = {
     find,
     findById,
     findSteps,
-
-
+    add,
+    update,
+    remove
 
 }
