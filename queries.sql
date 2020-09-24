51a5feb5686617cdfb5af4ca0ec03f4a89b28951
JenVest2020 <jenvest2019@gmail.com>
2020-09-23 19:23:59 -0500

    queries file and exit quiz completed

--- queries.sql
+++ queries.sql
@@ -9,7 +9,21 @@ select o.id,
     s.CompanyName
 from Order as o
     join Shipper as s
-where o.OrderDate < 'August 9, 2012';
+where o.OrderDate < '2012-08-09';
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
--- There is no quantity column in order table
+Select p.ProductName, od.Quantity
+from [OrderDetail] as od
+join [Product] as p
+where od.OrderId = '10251';
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+SELECT od.OrderId,
+       c.CompanyName,
+       e.LastName as EmployeeLastName
+  FROM [Order]
+       JOIN
+       Customer AS c
+       JOIN
+       Employee AS e
+       JOIN
+       OrderDetail AS od;
